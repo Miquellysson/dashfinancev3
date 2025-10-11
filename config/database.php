161d199d97146498b18e75dad5f226a5b1b1adb2
Mike Lins <miquellyssom@gmail.com>
2025-10-11 01:10:24 -0300

    config

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 
 <?php
 $host = 'localhost';
-$db   = 'u100060033_financav3';
-$user = 'u100060033_financav3';
+$db   = 'u100060033_financa_v3';
+$user = 'u100060033_financa_v3';
 $pass = 'Arkaleads2025!@#';
 $charset = 'utf8mb4';
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
